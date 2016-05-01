2372c2fc478957b4adbe8fa3973c580659451c4b
Brian Nickerson <nickerson.brian@gmail.com>
2016-04-30 22:57:06 -0400

    I swear I fixed it this time

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :quiz
   has_one :answer
-  validates :question, presence: true, length: { maximum:200 }
+  validates :question_asked, presence: true, length: { maximum:200 }
 end
